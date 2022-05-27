5f3e40875c24628f30a6b1edfb9cfd79eefef13f
Aof Sangsuwan <warning.2753@email.com>
2022-05-27 17:36:32 -0600

    add aprevGuesses Property to the game oject

--- app.js
+++ app.js
@@ -3,8 +3,14 @@ const game = {
   biggestNum: 100,
   smallestNum: 1,
   secretNum: null,
+  PrevGuesses: [],
+
+  
   play: function() {
     this.secretNum = Math.floor(Math.random() * 
       (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
   }
 }
+// console.log(game)
+// console.log(game.prevGuesses) 
+// game.getGuess()
