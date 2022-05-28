c3c5152604c3c700eb4b817afcc9f8cceb8cf524
Aof Sangsuwan <warning.2753@email.com>
2022-05-27 19:55:28 -0600

    #2 and #3 fix errors

--- app.js
+++ app.js
@@ -9,16 +9,15 @@ const game = {
   play: function() {
     this.secretNum = Math.floor(Math.random() * 
       (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
-      
-    },
-  getGuess: function(){
-   let play = parseInt(prompt(`Enter a guess between ${this.smallestNum} and ${this.biggestNum}`));
-   console.log(play)
-   if (typeof play === 'number'){
-     console.log(typeof play)
-   }
+      },
+      getGuess: function() {
+        let play = parseInt(prompt(`Enter a guess between ${this.smallestNum} and ${this.biggestNum}`))
+        console.log(play)
+        if (typeof play === 'number'){
+          console.log(typeof play)
+        }
   } 
 }
 // console.log(game)
 // console.log(game.prevGuesses) 
-game.getGuess()
+game.getGuess() 
