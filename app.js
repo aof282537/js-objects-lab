66d52ad83125b742e51ef8603b11e3d8c07d1702
Aof Sangsuwan <warning.2753@email.com>
2022-05-27 21:52:57 -0600

    done!!!!!

--- app.js
+++ app.js
@@ -5,34 +5,46 @@ const game = {
   secretNum: null,
   prevGuesses: [],
 
-  
-  play: function() {
-    this.secretNum = Math.floor(Math.random() * 
+
+  play: function () {
+    this.secretNum = Math.floor(Math.random() *
       (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
-      },
-      getGuess: function() {
-        let play = parseInt(prompt(`Enter a guess between ${this.smallestNum} and ${this.biggestNum}`))
-        console.log(play)
-        if (typeof play === 'number'){
-          console.log(typeof play)
-        }
-        while (isNaN(play) || play < this.smallestNum || play > this.biggestNum)
-        return play;
+      console.log(this.secretNum)
+
+    do {
+      this.prevGuesses.push(this.getGuess());
+      console.group(this.prevGuesses)
+      this.render();
+    } while (this.prevGuesses[this.prevGuesses.length - 1] !== this.secretNum)
+  //length how long is the array, -1 is I want the last items. 
   },
 
-  
-  render: function() {
-    let message; 
-    if (this.prevGuesses[this.prevGuesses.length-1] === this.secretNum) {
-      message = `Congrats! You guessed the number in ${this.prevGuesses.length}!`
-    } else if (this.prevGuesses[this.prevGuesses.length-1] > this.secretNum) {
+  getGuess: function () {
+    let play
+    // if (typeof play === 'number') {
+    //   console.log(typeof play)
+    // }
+    do {
+      play = parseInt(prompt(`Enter a guess between ${this.smallestNum} and ${this.biggestNum}`))
+      console.log(play)
+    } while (isNaN(play) || play < this.smallestNum || play > this.biggestNum)
+    return play;
+  },
+
+  render: function () {
+    let message;
+    console.log("Rendering")
+    if (this.prevGuesses[this.prevGuesses.length - 1] === this.secretNum) {
+      message = `Congrats! You guessed the number in ${this.prevGuesses.length} tries!!`
+    } else if (this.prevGuesses[this.prevGuesses.length - 1] > this.secretNum) {
       message = `Your guess is too high ${this.prevGuesses.join(',')}!`
-    } else if (this.prevGuesses[this.prevGuesses.length-1] < this.secretNum) {
+    } else if (this.prevGuesses[this.prevGuesses.length - 1] < this.secretNum) {
       message = `You guess is to low ${this.prevGuesses.join(',')}!`
     }
     alert(message)
-  } 
+  }
 }
 // console.log(game)
 // console.log(game.prevGuesses) 
-game.getGuess() 
+// game.getGuess() 
+game.play()
